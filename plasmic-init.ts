f83abf3e814ddeed20b1902ff6c81b7d7ce34b5b
achang-tcs <achang@truechoicesolutions.com>
2022-07-18 17:41:39 -0400

    feat: add slider component

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,5 +1,6 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
 import { HelloWorld } from './components/HelloWorld';
+import { Slider } from './components/Slider';
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
@@ -13,7 +14,7 @@ export const PLASMIC = initPlasmicLoader({
   // For development, you can set preview to true, which will use the unpublished
   // project, allowing you to see your designs without publishing.  Please
   // only use this for development, as this is significantly slower.
-  preview: false,
+  preview: true,
 });
 
 // You can register any code components that you want to use here; see
@@ -28,5 +29,12 @@ PLASMIC.registerComponent(HelloWorld, {
   props: {
     verbose: 'boolean',
     children: 'slot'
+  }
+});
+
+PLASMIC.registerComponent(Slider, {
+  name: 'Slider',
+  props: {
+    verbose: 'boolean'
   }
 });
